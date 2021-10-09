e3aa1beaa0ecad0124a37d1dd98222612fba0b49
Vineet <vineet98love@gmail.com>
2021-10-09 12:14:58 +0530

    update

--- block-BNaadl/blog/app.js
+++ block-BNaadl/blog/app.js
@@ -3,9 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var articlesRouter = require('./routes/articles');
+
+// connect to database
+mongoose.connect(
+    "mongodb://localhost/blog", 
+    { useNewUrlparser: true, useUnifiedTopology: true },
+     (err) => {
+         console.log(err ? err : "Connected to database");
+     }
+)
 
 var app = express();
 
@@ -19,8 +29,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/articles', articlesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
